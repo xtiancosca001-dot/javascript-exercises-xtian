6a38d77a75f3cb3e03caebe6bfddfaee29d7cf09
xtiancosca001-dot <christiancosca@gmail.com>
2025-07-02 21:48:36 +0800

    Update equation c

--- 04_mathEquations/mathEquations.js
+++ 04_mathEquations/mathEquations.js
@@ -1,6 +1,6 @@
 const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
 const b = 1 + 8;
-const c = "22 times three"
+const c = 22 * 3;
 const d = "the *remainder* of 5/4"
 const e = "the variable 'b' minus 17"
 const f = "the sum of the previous five variables"
