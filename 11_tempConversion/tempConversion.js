44d8d1ae0f91f8f44482c1e6264ce41688c07ae9
Christian Eric Cosca <ccosca@iripple.com>
2025-07-12 16:48:45 +0800

    Complete convertToFahrenheit method

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,10 +1,12 @@
 const convertToCelsius = function(degressF) {
-  const degreesC = (5 / 9) * (degressF - 32)
+  const degreesC = (5 / 9) * (degressF - 32);
   return Number( degreesC.toFixed(1) );
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(degreesC) {
+  const degreesF = (( 9 * degreesC ) / 5) + 32;
+  return Number( degreesF.toFixed(1) );
+}
 
 // Do not edit below this line
 module.exports = {
