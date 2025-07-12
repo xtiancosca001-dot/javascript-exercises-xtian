30bfad4bdcb2e8209c5577f6a147eaa39269bb0a
Christian Eric Cosca <ccosca@iripple.com>
2025-07-12 15:34:31 +0800

    Complete the removeFromArray Method

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for(const arg of args) {
+        arr = arr.filter(item => item !== arg);
+    }
+    return arr;
 };
 
 // Do not edit below this line
