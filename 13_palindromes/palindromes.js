885c8dd31615cebe011dc155d299af8c1823bb13
xtiancosca001-dot <christiancosca@gmail.com>
2025-07-19 18:41:04 +0800

    Complete palindrome function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const charIsLetter = char=>char!==' '&&char!=='!'&&char!=='.'&&char!==',';
+    const source = str.split('')
+                       .filter(char=>charIsLetter(char))
+                       .join('').toLowerCase();
+    const reversedString = source.split('')
+                                 .reverse()
+                                 .join('');
+    console.log(reversedString);
+    return source === reversedString;
 };
 
 // Do not edit below this line
