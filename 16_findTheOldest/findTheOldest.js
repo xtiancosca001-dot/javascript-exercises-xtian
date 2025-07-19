766189309e4c7f7816ecbcc23f51bd5c99b13dcd
xtiancosca001-dot <christiancosca@gmail.com>
2025-07-19 20:15:56 +0800

    Complete the oldest person function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (peopleArray) {
+    const yearToday = (new Date()).getFullYear();
+    return peopleArray.sort((b, a) => ((a.yearOfDeath || yearToday) - a.yearOfBirth) -
+        ((b.yearOfDeath || yearToday) - b.yearOfBirth))[0];
 };
 
 // Do not edit below this line
