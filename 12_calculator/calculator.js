acd109b323c00f9e6736db6c480a268048291e48
xtiancosca001-dot <christiancosca@gmail.com>
2025-07-19 18:10:10 +0800

    Complete factorial function

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -20,8 +20,9 @@ const power = function(num1, num2) {
 	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num < 1) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
