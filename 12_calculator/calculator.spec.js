20c0725bf89be503bd591b0193fcf2987958eeca
xtiancosca001-dot <christiancosca@gmail.com>
2025-07-19 18:01:19 +0800

    Complete subtract function

--- 12_calculator/calculator.spec.js
+++ 12_calculator/calculator.spec.js
@@ -15,15 +15,15 @@ describe('add', () => {
 });
 
 describe('subtract', () => {
-  test.skip('subtracts numbers', () => {
+  test('subtracts numbers', () => {
     expect(calculator.subtract(10, 4)).toBe(6);
   });
 
-  test.skip('subtracts negative numbers', () => {
+  test('subtracts negative numbers', () => {
     expect(calculator.subtract(-10, -4)).toBe(-6);
   });
 
-  test.skip('subtracts numbers of mixed parity', () => {
+  test('subtracts numbers of mixed parity', () => {
     expect(calculator.subtract(-8, 7)).toBe(-15);
   });
 });
